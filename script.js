c4f4918cb62af6de216d2d0c94d9100e12d5a3f8
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-11 10:22:24 +0530

    test

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-gsap.from(".imageContainer",{
-    width: "0%",
-    duration: 2,
-    ease: "expo.inOut",
-    stagger: 2
-})
+gsap.from(".imageContainer", {
+  width: "0%",
+  duration: 3,
+  ease: "expo.out",
+  stagger: 3,
+  repeat: -1
+});
+
+gsap.timeline({ repeat: -1 })
+  .to("#text h1", {
+    y: -70,
+    duration: 3,
+    delay: 3,
+    ease: "expo.out"
+  })
+  .to("#text h1", {
+    y: -140,
+    duration: 3,
+    ease: "expo.out"
+  });
