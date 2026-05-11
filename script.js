3e738e66d23604d9d90210231e7765ec13bf3df4
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-11 10:26:23 +0530

    easeInOut

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 gsap.from(".imageContainer", {
   width: "0%",
   duration: 3,
-  ease: "expo.out",
+  ease: "expo.easeInOut",
   stagger: 3,
   repeat: -1
 });
@@ -11,10 +11,10 @@ gsap.timeline({ repeat: -1 })
     y: -70,
     duration: 3,
     delay: 3,
-    ease: "expo.out"
+    ease: "expo.easeInOut"
   })
   .to("#text h1", {
     y: -140,
     duration: 3,
-    ease: "expo.out"
+    ease: "expo.easeInOut"
   });
